e78354f8b91e2c587ed7632352da42ebce6debf6
asdf1595 <alstjs23@gmail.com>
2016-05-22 14:45:09 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,110 @@
+$(function () {
+
+    // 스크롤 올리고 내릴때 보이게 안보이게
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+            $('header').removeClass('nav-down').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if (st + $(window).height() < $(document).height()) {
+                $('header').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+
+        lastScrollTop = st;
+    }
+
+    // 변수 높이값 지정
+//    var ht = $(window).height();
+//    // 브라우저 높이값을 section의 높으값으로
+//    $("section").height(ht);
+    
+//     var ht = $("section").height();
+//    $("section").height(ht);
+
+//    $(window).on('resize', function () {
+//        var ht = $(window).height();
+//        $("section").height(ht);
+//    });
+    
+//    $(window).on('resize', function () {
+//        var ht = $("section").height();
+//        $("section").height(ht);
+//    });
+
+    // 메뉴버튼클릭
+
+//    $("#menu>ul>li").on('click', function (e) {
+//        e.preventDefault()
+//
+//        var ht = $(window).height();
+//        var i = $(this).index();
+//        var nowTop = i * ht;
+//        $("html,body").stop().animate({
+//            "scrollTop": nowTop
+//        }, 1000);
+//    });
+    
+//    $("#menu>ul>li").on('click', function (e) {
+//        e.preventDefault()
+//
+//        var ht = $("section").height();
+//        var i = $(this).index();
+//        var nowTop = i * ht;
+//        $("html,body").stop().animate({
+//            "scrollTop": nowTop
+//        }, 1000);
+//    });
+    
+    // 마우스 휠 움직일때
+
+    $("section").on('mousewheel', function (event, delta) {
+        if (delta > 0) {
+            var prev = $(this).prev().offset().top;
+            $("html,body").stop().animate({
+                "scrollTop": prev
+            }, 1000, "easeInOutSine");
+        } else if (delta < 0) {
+            var next = $(this).next().offset().top;
+
+            $("html,body").stop().animate({
+                "scrollTop": next
+            }, 1000, "easeInOutSine");
+        }
+    });
+    
+    // 이미지 마우스 올렸을때
+    
+    /*var this = $(this);
+    
+    $(".workList>ul>li>a").on("mouseover",function(){
+        $(this).find("span").slideDown(500);
+        $(this).find("span").not().hide();
+    });*/
+    
+});
